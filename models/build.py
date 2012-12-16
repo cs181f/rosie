ffde3b67068521a7744e53112205e5752082e919
Jesse Pollak <jpollak92@gmail.com>
2012-12-16 12:55:29 -0800

    adds information to build necessary for github issue creation

--- models/build.py
+++ models/build.py
@@ -15,11 +15,11 @@ class BuildErrorException(Exception):
 # sets up the connection to the database.
 connection = Connection()
 
-# creates a schema that will be enforced by mongokit    
+# creates a schema that will be enforced by mongokit
 # MongoKit uses unicode: http://api.mongodb.org/python/current/tutorial.html#a-note-on-unicode-strings
 # This is for compliance with the BSON format that it stores data in.
 # As a result, all strings will say unicode instead of string.
-# We do not need to worry about any of this; MongoKit will handle 
+# We do not need to worry about any of this; MongoKit will handle
 # any string to unicode conversions necessary.
 @connection.register    # assigns the schema to the database
 class Build(Document):
@@ -28,17 +28,22 @@ class Build(Document):
     use_dot_notation = True
     dot_notation_warning = True
     structure = {
-        'repository': {     
-            'url': unicode,     # url to the github repository            
+        'repository': {
+            'url': unicode,     # url to the github repository
             'name': unicode,    # repository name
-            'description': unicode }, # description from github
+            'description': unicode,
+            'owner': {
+                'name': unicode,
+                'email': unicode
+            }
+        }, # description from github
         'url': unicode,         # url to specific commit
         'author': {             # author of commit
             'email': unicode,   # author's email
             'name': unicode },  # author's name
         'message': unicode,     # the commit message describing changes made
         'timestamp': unicode,   # time committed
-        'ref': unicode,         # branch information    
+        'ref': unicode,         # branch information
         'status': IS(0,1,2),    # status of the build attempt. must be one of these numbers.
         'error': unicode        # information about any build errors
     }
@@ -59,7 +64,7 @@ class Build(Document):
         'error',
     ]
 
-    
+
     # The following two methods are documented here:
     #    http://namlook.github.com/mongokit/json.html
     """
@@ -67,7 +72,7 @@ class Build(Document):
         provided by MongoKit
         returns a json version of the database object
     """
-    
+
     """
     self.from_json(json)
         provided by MongoKit
@@ -81,18 +86,18 @@ class Build(Document):
             and either fills it with json and saves it away
             or grabs the indicated Build from the database
         """
-        
+
         # if both optional fields are provided, raise an error
         if json is not None and id is not None:
             raise BuildErrorException("Cannot use both json and id args.")
-        
+
         # initializer for the parent Document class
         Document.__init__(self)
-        
+
         if json is not None:
             self.from_json(json) # provided by mongokit
             self.validate()      # provided by mongokit
-        
+
         # while we can always just use self.find, this cleans it up
         elif id is not None:
             matches = self.find({'_id': id})
@@ -105,12 +110,12 @@ class Build(Document):
         # note that if neither json nor id are provided, it
         # creates an empty Build with no data. This is useful
         # for testing mostly.
-    
+
     # save
     def save(self):
         """ Stores an object in the database.
             Returns the ID to store in the build queue
-            
+
             While MongoKit contains a save() method, we are
             bypassing it in favor of the PyMongo version in order
             to obtain the internal ID number, which we will use
@@ -118,27 +123,27 @@ class Build(Document):
         """
         # calls self.validate()
         self.validate()
-        
+
         # this is the PyMongo save method:
         return self.collection.save(self, safe=safe, *args, **kwargs)
 
     # update_with_results
     def update_with_results(self, results):
         self['error'] = results
-        
+
         self.save() # self.save() handles validation for me
-        
+
         # no return value
-    
+
     # The following methods are documented here:
     #    http://namlook.github.com/mongokit/query.html
-    
+
     """
     self.find({'_id': number})
         provided by MongoKit
         finds the build by ID number
     """
-    
+
     """
     self.find() # with no arguments
         provided by MongoKit
