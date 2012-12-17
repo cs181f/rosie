72c381268e1319998a4b5927a774abdd86e34db0
Jesse Pollak <jpollak92@gmail.com>
2012-12-16 18:46:19 -0800

    updates api to work well

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 """
-This module exposes all of the endpoints of the application programming 
+This module exposes all of the endpoints of the application programming
 interface which will be hit by the external parts of Rosie. This includes
 GitHub's Webhooks, the web frontend, and the command line interface.
 
@@ -8,70 +8,99 @@ triggering events, so the complexity of each method is fairly low.
 """
 
 from flask import Flask
-from flask import jsonify #translates a dictionary into JSON 
+from flask import jsonify #translates a dictionary into JSON
                           #(http://flask.pocoo.org/docs/api/)
-from flask import json #parses JSON                          
+from flask import (
+    json, #parses JSON
+    request
+)
+
 from models import (
     BuildQueue,
     Build,
-    WorkerThread
+    WorkerThread,
+    connection
 )
 
+from datetime import datetime
+from mongokit import ObjectId
+
 api = Flask(__name__)
-api.config.from_object('config')
+api.config.from_object('rosie.config')
 
-worker = None
-queue = BuildQueue()
+api.worker = None
+api.queue = BuildQueue()
 
+Build = connection.Build
 ###GitHub Webhook###
-
-@api.route('/build', methods=['POST']) 
-                     #The @api.route decorator is a flask function that 
-                     #triggers the method when that url is visited, with
-                     #access given to the paramaters passed in the URL.
-                     # http://flask.pocoo.org/docs/api/#api
-def build():  
+@api.route('/build', methods=['POST'])
+def build():
+    """
+    The @api.route decorator is a flask function that
+    triggers the method when that url is visited, with
+    access given to the paramaters passed in the URL.
+    http://flask.pocoo.org/docs/api/#api
+    """
     #creates Build object and stores it in the database by calling the public
     #constructor
-    build_id = Build(request.form['payload'])
-    
+    payload = request.json.get('payload', None)
+
+    if not payload:
+        return jsonify(success=False)
+
+    build = connection.Build(payload)
+    build['status'] = 0
+    build['error'] = ''
+    build['build_time'] = datetime.utcnow()
+    build.save()
+
     #stores the build_id in the build queue
-    queue.add_build(build_id)
-    configs = app.open_resource('')
-    if (worker is None or worker.current_build is None):
-        worker = Worker(queue, api.config)
-        worker.start()
+    api.queue.add_build(build)
+
+    if (api.worker is None or api.worker.current_build is None):
+        api.worker = WorkerThread(api.queue, api.config, connection)
+        api.worker.start()
+        api.worker.join()
+
+    return jsonify(success=True, id=str(build['_id']))
 
 ###HTML Endpoints###
 
 @api.route('/ping', methods=['GET'])
 def ping():
     #checks whether the worker is processing a build or free
-    if (worker is None or worker.current_build is None):
+    if not api.worker.is_building():
         return jsonify(dict(building=False))
     else:
-        return jsonify(dict(building=True, build=worker.current_build))
+        return jsonify(dict(building=True, build=api.worker.current_build))
     #returns jsonify(status of server)
 
-@api.route('/builds/<build_id>', methods=['POST'])
+@api.route('/builds/<build_id>', methods=['GET'])
 def get_build(build_id):
     #looks up the build by its id in the database
-    #the variable build_id is passed in the url 
-    build = Build.find_one(dict(_id=build_id))
+    #the variable build_id is passed in the url
+
+    try:
+        build = Build.get_from_id(ObjectId(build_id))
+    except Exception:
+        return jsonify(error="Invalid Build ID")
+
+    if build is None:
+        return jsonify(error="Invalid Build ID")
 
     #looks at the status of the build
     #returns jsonify(status of build)
-    return jsonify(build.to_json)
+    return (build.to_json(), 200)
 
-@api.route('/builds', methods=['POST'])
+@api.route('/builds', methods=['GET'])
 def get_builds():
     #gets all builds in DB using .find() method
     builds = []
     for build in Build.find():
-        builds.append(build)
+        builds.append(build.to_json())
 
     #returns data for all builds
-    return jsonify(builds)
+    return json.dumps(builds), 200
 
 @api.route('/check_settings', methods=['GET'])
 def get_settings():
@@ -80,26 +109,39 @@ def get_settings():
     #this is to populate the web interface and
     #to provide information at the command line
 
-@api.route('/blame', methods=['POST'])
+@api.route('/blame', methods=['GET'])
 def blame():
-    bad_people = dict(0)
+    bad_people = dict()
     for build in Build.find():
-        if build.error is not None:
-            bad_people[build.author.name] += 1
+        if build.status == 2:
+            if bad_people.get(build.author.name, None) is None:
+                bad_people[build.author.name] = 1
+            else:
+                bad_people[build.author.name] += 1
 
     return jsonify(bad_people)
 
 @api.route('/builds/new', methods=['POST'])
 def rebuild():
     #takes build ID as parameter
-    id = request.form['build_id']
+    id = request.form.get('build_id', None)
+
+    try:
+        build = Build.get_from_id(ObjectId(id))
+    except Exception:
+        return jsonify(error="Invalid Build ID")
+
+    if build is None:
+        return jsonify(error="Invalid Build ID")
     #looks up a build by that ID
     #rebuilds build to see if it fails new tests
-    queue.add_build(build_id)
-    configs = app.open_resource('')
-    if (worker is None or worker.current_build is None):
-        worker = Worker(queue, api.config)
-        worker.start()
+    api.queue.add_build(build)
+
+    if (api.worker is None or api.worker.current_build is None):
+        api.worker = WorkerThread(api.queue, api.config, connection)
+        api.worker.start()
+
+    return jsonify(success=True, id=id)
 
 if __name__ == '__main__':
     api.run()
